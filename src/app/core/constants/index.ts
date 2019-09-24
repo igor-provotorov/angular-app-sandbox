234f4dc6efed914fb1ffeed2745b540fadc7ff3a
igor-provotorov <garynya@yandex.ru>
2019-09-24 10:41:18 +0300

    feat: add check for error result (button 'load more' is't showing)

--- src/app/core/constants/index.ts
+++ src/app/core/constants/index.ts
@@ -3,9 +3,11 @@ const BASE_SEARCH_IMAGES_URL: string = "https://image.tmdb.org/t/p/w500";
 const BASE_YOUTUBE_VIDEOS_URL: string = "https://www.youtube.com/watch?v=";
 const API_KEY: string = "026c9d92e76619507fb31d8065f3304d";
 const PER_PAGE: number = 20;
+const PAGE: number = 1;
 const NO_ACTOR_IMAGE_URL: string =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/User-Pict-Profil.svg/400px-User-Pict-Profil.svg.png";
 const NO_SUCH_MOVIES: string = "No such movies";
+const ERROR: string = "#Error:";
 
 export {
     BASE_SEARCH_URL,
@@ -13,6 +15,8 @@ export {
     BASE_YOUTUBE_VIDEOS_URL,
     API_KEY,
     PER_PAGE,
+    PAGE,
     NO_ACTOR_IMAGE_URL,
     NO_SUCH_MOVIES,
+    ERROR,
 };
--- src/app/core/utils/check-empty-results.util.ts
+++ src/app/core/utils/check-empty-results.util.ts
@@ -1,10 +1,14 @@
-import { NO_SUCH_MOVIES } from "../constants/index";
+import { NO_SUCH_MOVIES, ERROR } from "../constants/index";
 import { ModifiedResultMovie } from "../services/search-films-service/models/index";
 
 /**
- * Check if results is empty.
+ * Check if results is empty or response is .
  */
-const checkEmptyResults: (param: Array<ModifiedResultMovie>) => boolean = (data: Array<ModifiedResultMovie>): boolean =>
-    data[0].title !== NO_SUCH_MOVIES;
+const checkEmptyResults: (param: Array<ModifiedResultMovie>) => boolean = (
+    data: Array<ModifiedResultMovie>
+): boolean => {
+    const res: boolean = data[0].title !== NO_SUCH_MOVIES && data[0].title.slice(0, 7) !== ERROR;
+    return res;
+};
 
 export { checkEmptyResults };
