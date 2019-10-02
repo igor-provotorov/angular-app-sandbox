d91bcf57262136d52649b4258804f1a17b393962
igor-provotorov <garynya@yandex.ru>
2019-10-02 10:39:46 +0300

    feat: remove non-used imports from navigation-menu.module

--- src/app/navigation-menu/navigation-menu.module.ts
+++ src/app/navigation-menu/navigation-menu.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
 
-import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { NavigationMenuComponent } from "./navigation-menu.component";
 import { AppRoutingModule } from "../app-routing.module";
 
 @NgModule({
     declarations: [NavigationMenuComponent],
-    imports: [
-        CommonModule,
-        FormsModule,
-        MatInputModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatProgressSpinnerModule,
-        AppRoutingModule,
-    ],
+    imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCheckboxModule, AppRoutingModule],
     providers: [],
     exports: [NavigationMenuComponent],
 })
