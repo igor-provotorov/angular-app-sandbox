b5798694e64c5b0b189edca8727afbacdce40007
igor-provotorov <garynya@yandex.ru>
2019-10-07 10:32:28 +0300

    feat: add store to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+
 import { ServiceModule } from "./services/service.module";
+import { reducers } from "./store/reducers";
 
 @NgModule({
-    imports: [ServiceModule],
+    imports: [ServiceModule, StoreModule.forRoot(reducers)],
 })
 export class CoreModule {}
