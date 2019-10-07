9a0d2fa45a172a201bca2c1b881dd6418512d305
igor-provotorov <garynya@yandex.ru>
2019-10-07 12:25:45 +0300

    feat: add index.ts in store/

---
+++ src/app/core/store/index.ts
@@ -0,0 +1,9 @@
+export {
+    FilmsToWatchActionTypes,
+    FilmsToWatchActions,
+    filmsToWatchReducer,
+    FilmsToWatchState,
+    selectFilmToWatchState,
+    selectFilmToWatchList,
+} from "./films-to-watch/index";
+export { State, reducers } from "./reducers";
