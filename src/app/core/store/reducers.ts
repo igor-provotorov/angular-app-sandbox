274a1884a7f39083453dbc4d549ec23b4299449a
igor-provotorov <garynya@yandex.ru>
2019-10-08 11:27:17 +0300

    feat: add interface for main state

--- src/app/core/store/reducers.ts
+++ src/app/core/store/reducers.ts
@@ -1,13 +1,7 @@
 import { ActionReducerMap } from "@ngrx/store";
 
-import { filmsToWatchReducer, FilmsToWatchState } from "./films-to-watch/index";
-
-/**
- * FilmsToWatch interface.
- */
-export interface State {
-    filmsToWatch: FilmsToWatchState;
-}
+import { filmsToWatchReducer } from "./films-to-watch/index";
+import { State } from "./state.model";
 
 /**
  * Reducers Map.
