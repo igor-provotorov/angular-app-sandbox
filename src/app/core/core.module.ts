a32532898e1ce2598f130eb1abf0fa183635adf8
igor-provotorov <garynya@yandex.ru>
2019-10-09 13:29:52 +0300

    feat: add StoreDevtoolsModule for debugging in redux dev tools

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,8 +4,16 @@ import { StoreModule } from "@ngrx/store";
 import { ServiceModule } from "./services/index";
 import { reducers } from "./store/index";
 import { StoreFacadeModule } from "./store-facades/index";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 @NgModule({
-    imports: [ServiceModule, StoreModule.forRoot(reducers), StoreFacadeModule],
+    imports: [
+        ServiceModule,
+        StoreModule.forRoot(reducers),
+        StoreFacadeModule,
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+        }),
+    ],
 })
 export class CoreModule {}
