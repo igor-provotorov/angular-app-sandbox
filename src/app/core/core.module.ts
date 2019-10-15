8a72844093b6a62fb8fec78963ebd38d139d9f35
igor-provotorov <garynya@yandex.ru>
2019-10-15 11:11:02 +0300

    feat: add initialization action, when the app will initialize

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from "@angular/core";
-import { StoreModule } from "@ngrx/store";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+
+import { StoreModule, Store } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
 
 import { ServiceModule } from "./services/index";
-import { reducers } from "./store/index";
+import { reducers, State, GetFilmsToWatch, FilmsToWatchEffects } from "./store/index";
 import { StoreFacadeModule } from "./store-facades/index";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../../environments/environment";
-
-import { EffectsModule } from "@ngrx/effects";
-import { FilmsToWatchEffects } from "./store/films-to-watch/films-to-watch.effect";
 
 @NgModule({
     imports: [
@@ -20,6 +19,18 @@ import { FilmsToWatchEffects } from "./store/films-to-watch/films-to-watch.effec
             logOnly: environment.production,
         }),
         EffectsModule.forRoot([FilmsToWatchEffects]),
+    ],
+    providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: (store: Store<State>): (() => void) => {
+                return (): void => {
+                    store.dispatch(new GetFilmsToWatch());
+                };
+            },
+            multi: true,
+            deps: [Store],
+        },
     ],
 })
 export class CoreModule {}
